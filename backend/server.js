886b132357ceb74c510e1f44141e16a1122b4f8d
Jeannenot Gauthier <jeannenot.gauthier@gmail.com>
2023-07-07 19:17:14 +0200

    installation du serveur express

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,14 @@
-import { Express } from "express";
+import express from "express";
+import 'dotenv/config';
 
-const app = Express()
 
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
