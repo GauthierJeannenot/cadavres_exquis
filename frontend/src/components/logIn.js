447460eac52297e3621f5096cb775296f076c604
Jeannenot Gauthier <jeannenot.gauthier@gmail.com>
2023-08-16 18:33:36 +0200

    ajout de la validation des données envoyées dansle form

--- frontend/src/components/logIn.js
+++ frontend/src/components/logIn.js
@@ -7,32 +7,50 @@ const LogIn = () => {
     const {
         register,
         handleSubmit,
+        formState: {errors}
     } = useForm()
 
     const onSubmit = async (data) => {
         await logInUser(data)
     }
 
-    return(
+    return (
         <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="email">Email</label>
-            <input
-            {...register('email', {required : true})}
-            type="text"
-            id="email"
-            placeholder="email"
-             />
-            <label htmlFor="name">Password</label>
-            <input 
-            {...register('password', {required: true})}
-            type="text"
-            id="password"
-            placeholder="password" />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email">Email</label>
+                <input
+                    {...register('email', {
+                        required: true, pattern:
+                        {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Invalid email format"
+                        }
+                    })}
 
-            <input type="submit"/>
-        </form>
-        
+                    type="text"
+                    id="email"
+                    placeholder="email"
+                />
+                {errors?.email && <p>{errors.email.message}</p>}
+                <label htmlFor="password">Password</label>
+                <input
+                    {...register('password', { 
+                        required: true, pattern: 
+                        {
+                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/, 
+                            message: "Must be at least 12 characters long, and a combination of uppercase letters, lowercase letters, numbers, and symbols" 
+                        } 
+                    })}
+
+                    type="text"
+                    id="password"
+                    placeholder="password" 
+                />
+                {errors?.password && <p>{errors.password.message}</p>}
+
+                <input type="submit" />
+            </form>
+
         </>
     )
 }
